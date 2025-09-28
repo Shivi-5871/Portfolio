04eaed4501e98ad41d708b108b9c7174ffc9b262
Shivi Agawal <shiviagarwal253@gmail.com>
2025-09-28 16:57:17 +0530

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,14 @@ module.exports = {
           '33%': { transform: 'scale(1)'},
           '66%': { transform: 'scale(1)'},
           '100%': { transform: 'scale(1)'},
+          },
         },
-      },
-      animation: {
-        blob: 'blob 10s infinite',
-      },
-      backgroundImage: {
-        'skills-gradient': 'linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%)',
-      },
+        animation: {
+          blob: 'blob 10s infinite',
+        },
+        backgroundImage: {
+          'skills-gradient': 'linear-gradient(38.73deg, rgba(13,148,136,0.15) 0%, rgba(13,148,136,0) 50%), linear-gradient(141.27deg, rgba(13,148,136,0) 50%, rgba(13,148,136,0.15) 100%)',
+        },
     },
   },
   plugins: [],
