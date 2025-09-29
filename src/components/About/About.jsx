732744deb51d8567fc6c713d55123851fda480de
Shivi Agawal <shiviagarwal253@gmail.com>
2025-09-29 17:39:34 +0530

    updated resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/12g23isKJZgKHGqc8G2OHQhmJbWtczyQD/view?usp=drive_link"
+            href="https://drive.google.com/file/d/16HQEkC-FTjkhO0J2ZfARry_P3vxsIPhf/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
